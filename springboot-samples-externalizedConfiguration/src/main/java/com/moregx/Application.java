55d318a8eee3816286a1fc504067e35ff420f2b0
robin <fsg900212@163.com>
2017-03-08 18:06:49 +0800

    order

--- springboot-samples-externalizedConfiguration/src/main/java/com/moregx/Application.java
+++ springboot-samples-externalizedConfiguration/src/main/java/com/moregx/Application.java
@@ -5,16 +5,21 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource({
 	"classpath:property-source.properties"
 })
-public class Application {
+@ServletComponentScan
+public class Application  {
 
+	
 	public static void main(String[] args) {
 		
+		
+		System.setProperty("name", "name in Java System properties  level 9");
 		SpringApplication application = new SpringApplication(Application.class);
 		Map<String,Object> defaultProperties = new HashMap<String,Object>();
 		defaultProperties.put("name", "name in application.setDefaultProperties level 17");
